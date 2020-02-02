fc88b77a1a93a1a5afb178a8004d4c938a403221
AraqueGD <camiaraquecaro17@gmail.com>
2020-02-02 13:11:57 -0500

    [ADD]Insertion Sort Algorithms and Ansawer Complexy

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+
+
+/**
+ * swap_node - Swaps two nodes
+ * @a: First node
+ * @b: Second node
+ * Return: Nothing
+ */
+
+void swap_node(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+
+/**
+ * insertion_sort_lis - Creates a doubly linked list from an array of integers
+ * @list: Recieves the List.
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current  = NULL;
+	listint_t *iter = NULL;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		iter = current;
+		current = current->next;
+		while (iter && iter->prev)
+		{
+			if (iter->prev->n > iter->n)
+			{
+				swap_node(iter->prev, iter);
+				if (!iter->prev)
+					*list = iter;
+				print_list(*list);
+			}
+			else
+				iter = iter->prev;
+		}
+	}
+}
